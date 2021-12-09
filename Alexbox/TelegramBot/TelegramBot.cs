dec37e3a858612f470ab1292b58c8827c98c3c64
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2021-12-09 10:51:22 +0500

    Added some API features for bot

--- Alexbox/TelegramBot/TelegramBot.cs
+++ Alexbox/TelegramBot/TelegramBot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 #pragma warning disable 618
@@ -13,32 +14,39 @@ namespace Alexbox.TelegramBot
     public static class TelegramBot
     {
         private const string Token = "5019524624:AAHqK_HiVu77AthmnbAFTYrJrBnAhwPv6Zc";
-        private static TelegramBotClient _client = new(Token);
-        public static Dictionary<string, HashSet<long>> _usersById = new();
+        private static readonly TelegramBotClient Client = new(Token);
+        private static readonly Dictionary<string, IList<User>> UsersById = new();
 
         public static void Run()
         {
-            _client.StartReceiving();
-            _client.OnMessage += BotClientOnOnMessage;
+            Client.StartReceiving();
+            Client.OnMessage += BotClientOnOnMessage;
         }
 
         private static void BotClientOnOnMessage(object sender, MessageEventArgs e)
         {
             var id = e.Message.Chat.Id;
+            var user = e.Message.From;
             var text = e.Message.Text;
-            if (e.Message.Type != MessageType.Text) _client.SendTextMessageAsync(id, "Бот распознает только текст");
+            if (e.Message.Type != MessageType.Text) Client.SendTextMessageAsync(id, "Бот распознает только текст");
             if (text == "/start")
             {
-                _client.SendTextMessageAsync(id, "Введите идентификатор вашей комнаты. Например: 0352");
+                Client.SendTextMessageAsync(id, "Введите идентификатор вашей комнаты. Например: 0352");
             }
-            else if (_usersById.ContainsKey(text))
+            else if (UsersById.ContainsKey(text))
             {
-                _usersById[text].Add(id);
-                _client.SendTextMessageAsync(id, $"Вы успешно подключены к комнате: {text}");
+                UsersById[text].Add(user);
+                Client.SendTextMessageAsync(id, $"Вы успешно подключены к комнате: {text}");
             }
-            else _client.SendTextMessageAsync(id, "Узнайте идентификатор комнаты у хоста");
-
+            else Client.SendTextMessageAsync(id, "Узнайте идентификатор комнаты у хоста");
         }
+
+        public static IList<User> GetUsersByLobbyId(string lobbyId) =>
+            UsersById.ContainsKey(lobbyId) ? UsersById[lobbyId] : new List<User>();
+
+        public static void DeleteLobby(string id) => UsersById.Remove(id);
+
+        public static void SendMessageToUser(long id, string message) => Client.SendTextMessageAsync(id, message);
 
         public static string GenerateLobbyId()
         {
@@ -50,9 +58,9 @@ namespace Alexbox.TelegramBot
             }
 
             var result = stringBuilder.ToString();
-            if (_usersById.ContainsKey(result))
+            if (UsersById.ContainsKey(result))
                 return GenerateLobbyId();
-            _usersById.Add(result, new HashSet<long>());
+            UsersById.Add(result, new List<User>());
             return stringBuilder.ToString();
         }
     }
