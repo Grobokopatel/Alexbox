da7173bfc5fe705cdef069d1ef3c06ce9c7b7d29
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2022-01-07 17:34:22 +0500

    1

--- Alexbox/Domain/Stage.cs
+++ Alexbox/Domain/Stage.cs
@@ -5,16 +5,12 @@ namespace Alexbox.Domain
     public class Stage
     {
         private Func<int, int, int, int> ScoreFormula { get; set; }
-        public bool ShowRoundResults { get; private set; }
         public int TaskPerPlayer { get; private set; }
         public int GroupSize { get; private set; }
-        public string[] Captions { get; private set; }
-        public bool WaitForVotes { get; set; }
         public bool ShowScores { get; private set; }
         public string Paragraph { get; private set; }
         public int TimeOutInMs { get; private set; }
         public int? ShowRoundSubmits { get; private set; }
-        public bool WaitForReplies { get; set; }
         public bool SendingTasks { get; set; }
 
         public Distribution<long, Task> Distribution { get; set; }
@@ -31,13 +27,7 @@ namespace Alexbox.Domain
             ShowRoundSubmits = roundNumber;
             return this;
         }
-
-        public Stage WithScores()
-        {
-            ShowScores = true;
-            return this;
-        }
-
+        
         public Stage WithParagraph(string paragraph)
         {
             Paragraph = paragraph;
